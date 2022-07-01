51b481dbffa66e5787cc84775abf6e0456c38b36
mason <masonhayes2@protonmail.com>
2022-07-02 00:16:06 +0100

    improve raindrops.jl

--- julia/raindrops/raindrops.jl
+++ julia/raindrops/raindrops.jl
@@ -1,17 +1,11 @@
 function raindrops(number)
     result, sounds = "", ["Pling", "Plang", "Plong"]
-    my_modulo = map(x -> number % x, [3,5,7])
+    remainders = map(x -> number % x, [3,5,7])
 
-    for i in 1:length(my_modulo)
-        if my_modulo[i] == 0
-            result = join([result, sounds[i]])
-        end
+    for (remainder, sound) in zip(remainders, sounds)
+        remainder == 0 ? result = join([result, sound]) : ()
     end
-    if result > ""
-        return result
-    else 
-        return "$number"
-    end
+    result > "" ? result : "$number"
 end
 
 @time raindrops.([155, 441, 3531, 491*7*5])
