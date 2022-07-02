927b7c7f62fc08f48711b66e6be7b7e297c97ef2
mason <masonhayes2@protonmail.com>
2022-07-02 19:32:24 +0100

    rotational-cipher.jl iteration 2

--- julia/rotational-cipher/rotational-cipher.jl
+++ julia/rotational-cipher/rotational-cipher.jl
@@ -1,41 +1,16 @@
 
 # Create a basic rotational cipher
 
-function rotate(key::Int, text)
-    # Rotate the key to a positive integer between 0 and 26
-    key = key % 26
-    alphabet = 'a':'z'
-    Alphabet = 'A':'Z'
-    ciphertext = ""
-
-    for char in collect(text)
-        if char in alphabet
-            index = indexin(char, alphabet)
-            if index[1] + key > 26
-                ciphertext *= alphabet[index[1] + key - 26]
-            else
-                ciphertext *= alphabet[index[1] + key]
-            end
-        elseif char in Alphabet 
-            index = indexin(char, Alphabet)
-            if index[1] + key > 26
-                ciphertext *= Alphabet[index[1] + key - 26]
-            else
-                ciphertext *= Alphabet[index[1] + key]
-            end
-        else
-            ciphertext *= char
-        end
-    end
-    if typeof(text) == Char
-        return collect(ciphertext)[1]
-    elseif typeof(text) == String
-        return ciphertext
+function rotate(key::Int, char::Char)
+    if islowercase(char)
+        'a' + (char - 'a' + key) % 26
+    elseif isuppercase(char)
+        'A' + (char -'A' + key) % 26
+    else
+        char
     end
 end
-    
-# Test the rotational cipher
 
-rotate(13, "The quick brown fox jumps over the lazy dog, bro.")
+# Use multiple dispatch to call the above function for a String
 
-rotate(14, "m")
+rotate(key, str::String) = map(char -> rotate(key, char), str)
