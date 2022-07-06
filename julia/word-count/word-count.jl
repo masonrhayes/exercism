610f14bbe02f5b7fd5cfb760f24f383505c275ba
mason <masonhayes2@protonmail.com>
2022-07-06 18:05:13 +0100

    another iteration of word-count.jl

--- julia/word-count/word-count.jl
+++ julia/word-count/word-count.jl
@@ -2,25 +2,16 @@ using StatsBase
 
 function wordcount(sentence)
     
-    words = split(sentence, !acceptable_character, keepempty = false) .|> 
-        lowercase
-
-    for i in eachindex(words)
-        words[i] = strip(words[i], [''', '\n', '\t', ' '])
-    end
-    words != [""] ? countmap(words) : Dict{Any, Any}()
+    words = split(lowercase(sentence), !acceptable_character, keepempty = false)
+    
+    filter(!isempty, # No empty elements counted as words
+        strip.(ispunct, # Remove any leading or trailing punctuation of each word
+            words)) |> 
+                countmap # Count the number of times each word appears
 end
 
 # A function to filter out non-alphanumeric characters
 function acceptable_character(input)
-    if isletter(input)
-        return true
-    elseif isnumeric(input)
-        return true
-    elseif input == '''
-        return true
-    else
-        return false
-    end
+    return isletter(input) || isnumeric(input) || input == '''
 end
 
